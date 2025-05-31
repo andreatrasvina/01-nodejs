46aff54e60d2406f5fc8a0e582db23aeb0424912
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-30 20:48:50 -0700

    Revert "se me olvido hacer push jeje"
    This reverts commit b450b073f28b718b1199398ddbfd7297088158db.

--- cjs/index.js
+++
@@ -1,6 +0,0 @@
-//Common JS require module import
-const {sum} = require('./sum');
-
-//console.log(globalThis);
-
-console.log(sum(1,2));
--- mjs/index.mjs
+++
@@ -1,9 +0,0 @@
-// .js --> por defecto utiliza commonJS
-// .mjs --> para utilizar ES Modules
-// .cjs --> forzar utilizar CommonJS
-
-import {div, mult, sum} from './sum.mjs';
-
-console.log(sum(1,2));
-console.log(mult(1,2));
-console.log(div(1,2));
