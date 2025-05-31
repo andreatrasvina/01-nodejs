1bcda3342e4c0ea297039fd8314f3b059cf70cf4
Andrea Lucero <alucerotrasvina@gmail.com>
2025-05-30 23:51:08 -0700

    picocolors

--- 8-ls-advanced.js
+++ 8-ls-advanced.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs/promises');
-
 const path = require('node:path');
+const picocolors = require('picocolors');
 
 const folder = process.argv[2] ?? '.';
 
@@ -10,7 +10,7 @@ async function ls(folder){
     try{
         files = await fs.readdir(folder);
     }catch{
-        console.error(`no se pudo leer el directorio ${folder}`);
+        console.error(picocolors.red(`no se pudo leer el directorio ${folder}`));
         process.exit(1);
     }
 
@@ -31,7 +31,8 @@ async function ls(folder){
         const fileSize = stats.size.toString();
         const fileModified = stats.mtime.toLocaleString();
 
-        return `${fileType} ${file.padEnd(40)} ${fileSize.padStart(10)} ${fileModified}`
+        return `${fileType} ${picocolors.blue(file.padEnd(40)) } ${picocolors.green(fileSize.padStart(10))} ${picocolors.bgMagenta(fileModified)}`
+
     });
     
     const filesInfo = await Promise.all(filesPromises);
